220fe970c0bb521c8ddac59c0e623a48cffded27
abhisheknair10 <nairabs10@gmail.com>
2024-09-05 01:50:27 +0400

    wtf, im alive1

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Llama3.cu
+# LLM.cu - A LLaMa3-8B CUDA Inference Engine
+
+LLM.cu is a CUDA native implementation of the LLaMa3 architecture for sequence to sequence language modeling. Core principles of the transformer architecture from the papers [Attention is All You Need](https://arxiv.org/abs/1706.03762) and [LLaMA: Open and Efficient Foundation Language Models](https://arxiv.org/abs/2302.13971) are implemented using custom CUDA kernel definitions, facilitating scalable parallel processing on Nvidia GPUs.
+
+## Implementation Details
+
+## Usage
+
+```bash
+# Install the model weights 
+huggingface-cli download meta-llama/Meta-Llama-3-8B --local-dir ./model_weights/ --token $HF_TOKEN
+```
---
+++ src/Llama3Weight/Llama3Weight.h
@@ -0,0 +1,37 @@
+#pragma once
+
+// #include <cuda_fp16.h>
+#include <stdint.h>
+
+typedef struct {
+    int ndim;
+    int *shape;
+    long mem_len;
+    uint16_t *tensor;
+    // half *half_tensor;
+} Tensor;
+
+typedef struct {
+    int layernum;
+    Tensor *input_layernorm;
+    Tensor *mlp_down_proj;
+    Tensor *mlp_gate_proj;
+    Tensor *mlp_up_proj;
+    Tensor *post_attention_layernorm;
+    Tensor *self_attn_k_proj;
+    Tensor *self_attn_o_proj;
+    Tensor *self_attn_q_proj;
+    Tensor *self_attn_v_proj;
+} Llama3Layer;
+
+typedef struct {
+    Tensor *embed_tokens;
+    Tensor *lm_head;
+    Tensor *norm;
+    Llama3Layer **layers;
+    int n_layers;
+} Llama3;
+
+Llama3 *init_LLaMa3(int n_layers);
+
+void free_LLaMa3(Llama3 *llama3);
