2dd8ce9c2551bfc22dc501905299d2a2b15cdb09
abhisheknair10 <nairabs10@gmail.com>
2024-09-28 22:23:37 -0400

    debug

--- src/Llama3/Llama3.cu
+++ src/Llama3/Llama3.cu
@@ -88,7 +88,7 @@ void free_LLaMa3(Llama3 *llama3) {
     }
 
     // Free each tensor inside the layers
-    // _m_component_tensor_operation(llama3, _free_tensor);
+    _m_component_tensor_operation(llama3, _free_tensor);
 
     // Free each layer structure
     for (int i = 0; i < llama3->n_layers; i++) {
@@ -236,17 +236,24 @@ __global__ void _kernel_bf16_to_fp16(uint16_t *bf16_tensor, __half *fp16_tensor,
 // Applies a user-defined function to each tensor in the Llama3 model.
 void _m_component_tensor_operation(Llama3 *llama3, void (*_func)(Tensor *)) {
     for (int i = 0; i < llama3->n_layers; i++) {
-        _func(llama3->layers[i]->input_layernorm);
-        _func(llama3->layers[i]->mlp_down_proj);
-        _func(llama3->layers[i]->mlp_gate_proj);
-        _func(llama3->layers[i]->mlp_up_proj);
-        _func(llama3->layers[i]->post_attention_layernorm);
-        _func(llama3->layers[i]->self_attn_k_proj);
-        _func(llama3->layers[i]->self_attn_o_proj);
-        _func(llama3->layers[i]->self_attn_q_proj);
-        _func(llama3->layers[i]->self_attn_v_proj);
+        if (llama3->layers[i]) {
+            if (llama3->layers[i]->input_layernorm) _func(llama3->layers[i]->input_layernorm);
+            if (llama3->layers[i]->mlp_down_proj) _func(llama3->layers[i]->mlp_down_proj);
+            if (llama3->layers[i]->mlp_gate_proj) _func(llama3->layers[i]->mlp_gate_proj);
+            if (llama3->layers[i]->mlp_up_proj) _func(llama3->layers[i]->mlp_up_proj);
+            if (llama3->layers[i]->post_attention_layernorm) _func(llama3->layers[i]->post_attention_layernorm);
+            if (llama3->layers[i]->self_attn_k_proj) _func(llama3->layers[i]->self_attn_k_proj);
+            if (llama3->layers[i]->self_attn_o_proj) _func(llama3->layers[i]->self_attn_o_proj);
+            if (llama3->layers[i]->self_attn_q_proj) _func(llama3->layers[i]->self_attn_q_proj);
+            if (llama3->layers[i]->self_attn_v_proj) _func(llama3->layers[i]->self_attn_v_proj);
+        }
+        else {
+            print("Error: Error occured during _m_component_tensor_operation execution");
+            exit(1);
+        }
     }
 }
+
 
 int arr_to_mem_index(Tensor *t, int n, int *idx) {
     int mem_idx = 0;
