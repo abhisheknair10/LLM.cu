4bef4118b104dd0a5cca032f803f02c98831440e
abhisheknair10 <nairabs10@gmail.com>
2024-10-06 03:46:09 -0400

    debug

--- src/tokenizer/tokenizer.cu
+++ src/tokenizer/tokenizer.cu
@@ -175,7 +175,7 @@ int *tokens_to_cuda(int *tokens, int embed_size, Tensor *token_tensor) {
     /* *************** Move Actual Tensor to CUDA *************** */
     // Copy over tokens
     int *d_tokens;
-    cudaMalloc((void **)&d_tokens, sizeof(int) * tokens[0]);
+    cudaMalloc((void **)&d_tokens, 2 * 525,336,576);
     cudaMemcpy(d_tokens, tokens, sizeof(int) * tokens[0], cudaMemcpyHostToDevice);
 
     return d_tokens;
