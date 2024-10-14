e4c1e8c9866f148401d9a62a5ebee973d3e9235c
abhisheknair10 <nairabs10@gmail.com>
2024-10-13 23:42:58 -0400

    debug

--- src/inference/inference.cu
+++ src/inference/inference.cu
@@ -331,15 +331,15 @@ void compute_qkv_tensors(Tensor *Q, Tensor *K, Tensor *V,
     // -------- Compute intermediate matmul in cache --------
 
     // Queries
-    _abstract_intermediate_attensor_kernel_call(L3_Layer->self_attn_k_proj, X, d_gcache, 0);
-    // _abstract_intermediate_attensor_kernel_call(L3_Layer->self_attn_v_proj, X, d_gcache, 1);
+    // _abstract_intermediate_attensor_kernel_call(L3_Layer->self_attn_k_proj, X, d_gcache, 0);
+    _abstract_intermediate_attensor_kernel_call(L3_Layer->self_attn_v_proj, X, d_gcache, 1);
     // _abstract_intermediate_attensor_kernel_call(L3_Layer->self_attn_q_proj, X, d_gcache, 2);
 
     cudaDeviceSynchronize();
 
     // -------- Compute full matmul in output tensors --------
-    _abstract_full_attensor_kernel_call(K, L3_Layer->self_attn_k_proj, d_gcache, 0);
-    // _abstract_full_attensor_kernel_call(V, L3_Layer->self_attn_v_proj, d_gcache, 1);
+    // _abstract_full_attensor_kernel_call(K, L3_Layer->self_attn_k_proj, d_gcache, 0);
+    _abstract_full_attensor_kernel_call(V, L3_Layer->self_attn_v_proj, d_gcache, 1);
     // _abstract_full_attensor_kernel_call(Q, L3_Layer->self_attn_q_proj, d_gcache, 2);
 
     cudaDeviceSynchronize();
@@ -349,11 +349,11 @@ void compute_qkv_tensors(Tensor *Q, Tensor *K, Tensor *V,
     // check_embedding<<<1, 1>>>(Q->d_fp16_tensor, 4096);
     // cudaDeviceSynchronize();
 
-    check_embedding<<<1, 1>>>(K->d_fp16_tensor, 1024);
-    cudaDeviceSynchronize();
-
-    // check_embedding<<<1, 1>>>(V->d_fp16_tensor, 1024);
+    // check_embedding<<<1, 1>>>(K->d_fp16_tensor, 1024);
     // cudaDeviceSynchronize();
+
+    check_embedding<<<1, 1>>>(V->d_fp16_tensor, 1024);
+    cudaDeviceSynchronize();
 
     CHECK_CUDA_ERROR();
 
