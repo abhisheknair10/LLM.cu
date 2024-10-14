348750450310f3fb8216787b20d9390b0a61a45a
abhisheknair10 <nairabs10@gmail.com>
2024-10-13 22:57:01 -0400

    debug

--- src/inference/inference.cu
+++ src/inference/inference.cu
@@ -332,13 +332,17 @@ void compute_qkv_tensors(Tensor *Q, Tensor *K, Tensor *V,
 
     // Queries
     _abstract_intermediate_attensor_kernel_call(L3_Layer->self_attn_k_proj, X, d_gcache, 0);
+    cudaDeviceSynchronize();
     _abstract_intermediate_attensor_kernel_call(L3_Layer->self_attn_v_proj, X, d_gcache, 1);
+    cudaDeviceSynchronize();
     _abstract_intermediate_attensor_kernel_call(L3_Layer->self_attn_q_proj, X, d_gcache, 2);
     cudaDeviceSynchronize();
 
     // -------- Compute full matmul in output tensorss --------
     _abstract_full_attensor_kernel_call(K, L3_Layer->self_attn_k_proj, X, d_gcache, 0);
+    cudaDeviceSynchronize();
     _abstract_full_attensor_kernel_call(V, L3_Layer->self_attn_v_proj, X, d_gcache, 1);
+    cudaDeviceSynchronize();
     _abstract_full_attensor_kernel_call(Q, L3_Layer->self_attn_q_proj, X, d_gcache, 2);
     cudaDeviceSynchronize();
 
