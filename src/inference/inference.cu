ef24928c3a9f907af4423a9ff1013fd92927c97f
abhisheknair10 <nairabs10@gmail.com>
2024-11-01 16:51:40 -0400

    debug

--- src/inference/inference.cu
+++ src/inference/inference.cu
@@ -368,7 +368,7 @@ __global__ void kernel_compute_rms_norm(__half *RMSNorm, __half *X) {
 
     ((uint64_t *)X)[token_idx * 1024 + vw_embed_idx] = vec_x;
 
-    printf("%f\n", rms);
+    // printf("%f\n", rms);
     // printf("%f\n", __half2float(vec_x_y));
     // printf("%f\n", __half2float(vec_x_x));
     // printf("%f\n", __half2float(vec_x_z));
