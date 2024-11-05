6651a93fe8a284789c5525eb478ad0f9ba4230c7
abhisheknair10 <nairabs10@gmail.com>
2024-11-04 23:34:18 -0500

    debug

--- src/inference/inference.cu
+++ src/inference/inference.cu
@@ -412,10 +412,10 @@ __global__ void kernel_standard_tiled_gemm(
 
         // Load tile of Transform into shared memory
         if (col < n && (t * TILE_SIZE + threadIdx.x) < k) {
-            int T_idx = (t * TILE_SIZE + threadIdx.y) * n + col;
-            T_shmem[threadIdx.y * TILE_SIZE + threadIdx.x] = __half2float(Transform[T_idx]);
+            int T_idx = col * k + t * TILE_SIZE + threadIdx.x;
+            T_shmem[threadIdx.x * TILE_SIZE + threadIdx.y] = __half2float(Transform[T_idx]);
         } else {
-            T_shmem[threadIdx.y * TILE_SIZE + threadIdx.x] = 0.0f;
+            T_shmem[threadIdx.x * TILE_SIZE + threadIdx.y] = 0.0f;
         }
         __syncthreads();
 
