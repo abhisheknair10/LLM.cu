199ca2f5cc4afc388d0c13d8af4ee68211698f59
abhisheknair10 <nairabs10@gmail.com>
2024-11-08 01:34:23 -0500

    debug

--- src/inference/inference.cu
+++ src/inference/inference.cu
@@ -775,7 +775,7 @@ __global__ void kernel_compute_resolved_value_from_attention_score_tiled_matmul(
     int col = blockIdx.x * TILE_SIZE + threadIdx.x;
 
     float value = 0.0f;
-    for (int t = 0; t < (n + TILE_SIZE - 1) / TILE_SIZE; ++t) {
+    for (int t = 0; t < (k + TILE_SIZE - 1) / TILE_SIZE; ++t) {
         // Load attention_scores into shared memory
         if (row < m && (t * TILE_SIZE + threadIdx.x) < k) {
             int attn_idx = q_head_idx * m * k + row * k + (t * TILE_SIZE + threadIdx.x);
@@ -786,7 +786,7 @@ __global__ void kernel_compute_resolved_value_from_attention_score_tiled_matmul(
 
         // Load V into shared memory
         if (col < n && (t * TILE_SIZE + threadIdx.y) < k) {
-            int V_idx = col * (kv_heads * k) + (kv_head_idx * k) + t * TILE_SIZE + threadIdx.x;
+            int V_idx = (t * TILE_SIZE + threadIdx.y) * (kv_heads * n) + kv_head_idx * n + col;
             V_shmem[threadIdx.y * TILE_SIZE + threadIdx.x] = __half2float(V[V_idx]);
         } else {
             V_shmem[threadIdx.y * TILE_SIZE + threadIdx.x] = 0.0f;
