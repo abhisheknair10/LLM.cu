7fa34804d2899806d3e5e4a3163df9d13a34f8dd
abhisheknair10 <nairabs10@gmail.com>
2024-11-08 06:05:41 -0500

    debug

--- tests/gemm.py
+++ tests/gemm.py
@@ -6,6 +6,7 @@ k = 8
 
 a = torch.tensor(list(range(0, m * k))).reshape(m, k)
 b = torch.tensor(list(range(0, n * k))).reshape(n, k)
-b = b.transpose(0, 1)
 
-print(torch.matmul(a, b))
+print(a)
+print(b)
+print(torch.matmul(a, b.t()))
