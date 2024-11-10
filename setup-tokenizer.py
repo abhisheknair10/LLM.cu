9534006de15fb574547d25e0e3cee8ea0b952211
abhisheknair10 <nairabs10@gmail.com>
2024-11-10 02:40:22 -0500

    debug

--- setup-tokenizer.py
+++ setup-tokenizer.py
@@ -5,7 +5,7 @@ with open('model_weights/tokenizer.json', 'r') as f:
     tokenizer_str = f.read()
 
 # Replace 'Ġ' (represented as \u0120) with a space in the JSON string
-modified_tokenizer_str = tokenizer_str.replace("\u0120", " ").replace(r"\\n", r"\n")
+modified_tokenizer_str = tokenizer_str.replace("\u0120", " ").replace(r"\\n", r"\n").replace("\u010A", "\n")
 
 # Save the modified string back to the file
 with open('model_weights/modified_tokenizer.json', 'w') as f:
