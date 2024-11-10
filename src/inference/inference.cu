2237e9cf8d18fe1bce5cdaa6e77ff86003060a7f
abhisheknair10 <nairabs10@gmail.com>
2024-11-10 03:10:24 -0500

    debug

--- src/inference/inference.cu
+++ src/inference/inference.cu
@@ -157,7 +157,7 @@ void inference(Llama3 *llama3_model, Tensor *X, int *d_tokens, int *h_tokens, Cu
         compute_qkv_tensors(Cache->Q, Cache->K, Cache->V, llama3_model->layers[i], X);
 
         // RoPE scaling
-        // rope_scaling(Cache->Q, Cache->K);
+        rope_scaling(Cache->Q, Cache->K);
 
         // Attention computation
         compute_attention(X, Cache->Q, Cache->K, Cache->V, Cache);
@@ -583,7 +583,7 @@ __global__ void kernel_rope_scaling(__half *tensor, int transformed_embed_size, 
     __half2 h2_val = ((const __half2 *)tensor)[token_idx * transformed_embed_size + window_idx];
 
     const float scaling_factor = 10000.0f;
-    float theta = token_idx / powf(scaling_factor, ((float)window_idx) / ((float)transformed_embed_size));
+    float theta = (token_idx + 1) / powf(scaling_factor, ((float)window_idx) / ((float)transformed_embed_size));
     float cos_comp = cosf(theta);
     float sin_comp = sinf(theta);
 
