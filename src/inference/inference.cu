7180c920d2d82d8d98207aa2c155b0b2830b3660
abhisheknair10 <nairabs10@gmail.com>
2024-11-10 06:45:48 -0500

    debug

--- src/inference/inference.cu
+++ src/inference/inference.cu
@@ -137,6 +137,7 @@ CudaCache *init_cache(Llama3 *llama3_model) {
 /* ********************************* Inference Code ********************************* */
 int inference(Llama3 *llama3_model, Tensor *X, int *d_tokens, int *h_tokens, CudaCache *Cache) {
     CHECK_CUDA_ERROR();
+    cudaMemset(d_tokens, 0, sizeof(int) * (h_NUM_TOKENS + 1));
     cudaMemcpy(d_tokens, h_tokens, sizeof(int) * h_tokens[0], cudaMemcpyHostToDevice);
     CHECK_CUDA_ERROR();
 
