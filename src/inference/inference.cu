7e74a6d51e3d361c344f914335c2ebf5fbc15568
abhisheknair10 <nairabs10@gmail.com>
2024-11-10 06:54:45 -0500

    debug

--- src/inference/inference.cu
+++ src/inference/inference.cu
@@ -138,7 +138,7 @@ CudaCache *init_cache(Llama3 *llama3_model) {
 int inference(Llama3 *llama3_model, Tensor *X, int *d_tokens, int *h_tokens, CudaCache *Cache) {
     CHECK_CUDA_ERROR();
     printf("here it is: %d\n", h_tokens[0]);
-    cudaMemcpy(d_tokens, h_tokens, sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_tokens, h_tokens, sizeof(int) * h_tokens[0], cudaMemcpyHostToDevice);
     CHECK_CUDA_ERROR();
 
     // Set NUM_TOKENS value in device memory
