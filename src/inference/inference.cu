5c086f28c5881dd46688eba9389446082039fe4b
abhisheknair10 <nairabs10@gmail.com>
2024-11-10 01:16:22 -0500

    debug

--- src/inference/inference.cu
+++ src/inference/inference.cu
@@ -574,7 +574,7 @@ __global__ void kernel_rope_scaling(__half *tensor, int transformed_embed_size, 
     // Each thread loads 2 __half (each 2 bytes), as one 4 byte value into half2 datatype
     __half2 h2_val = ((const __half2 *)tensor)[token_idx * transformed_embed_size + window_idx];
 
-    const float scaling_factor = 500000.0f;
+    const float scaling_factor = 10000.0f;
     float theta = token_idx / powf(scaling_factor, ((float)window_idx) / ((float)transformed_embed_size));
     float cos_comp = cosf(theta);
     float sin_comp = sinf(theta);
@@ -698,9 +698,6 @@ __global__ void kernel_masking_softmax(float *attention_scores, int num_tokens) 
 
     int token_idx_y = blockIdx.x;
     int head_idx = blockIdx.y;
-
-    if (head_idx >= 32) return;
-    if (token_idx_y >= num_tokens) return;
 
     int token_idx_x;
     float exp_sum = 0.0f;
