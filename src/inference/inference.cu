2ac4d90d1121159c967652412adaedd949a81414
abhisheknair10 <nairabs10@gmail.com>
2024-11-10 03:21:31 -0500

    debug

--- src/inference/inference.cu
+++ src/inference/inference.cu
@@ -583,7 +583,7 @@ __global__ void kernel_rope_scaling(__half *tensor, int transformed_embed_size, 
     __half2 h2_val = ((const __half2 *)tensor)[token_idx * transformed_embed_size + embed_idx];
 
     const float scaling_factor = 500000.0f;
-    float theta = (token_idx + 1) / powf(scaling_factor, (2.0f * (float)embed_idx) / ((float)transformed_embed_size));
+    float theta = (token_idx + 1) / powf(scaling_factor, ((float)embed_idx) / ((float)transformed_embed_size));
     float cos_comp = cosf(theta);
     float sin_comp = sinf(theta);
 
