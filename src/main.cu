7b323a505c006d0ac79196d823733be48ac2a066
abhisheknair10 <nairabs10@gmail.com>
2024-11-10 06:19:41 -0500

    debug

--- src/main.cu
+++ src/main.cu
@@ -85,7 +85,7 @@ int main() {
             tokens[tokens[0]] = next_token;
             tokens[0] += 1;
 
-            for (int i = 1; i <= tokens[0]; i++) {
+            for (int i = 1; i < tokens[0]; i++) {
                 printf("%d, ", tokens[i]);
             }
             printf("\n");
