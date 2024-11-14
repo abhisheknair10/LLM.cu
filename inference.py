50dc8b798f5b5908ced83bf42426d2fd7dd7ef8e
abhisheknair10 <nairabs10@gmail.com>
2024-11-14 16:04:43 -0500

    debug

--- inference.py
+++ inference.py
@@ -15,7 +15,7 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name).half().to(device)
 
 # Define input text
-input_text = "What is the largest ocean in the world?\n\n"
+input_text = "What is the largest ocean in the world?\n\nThe"
 X = tokenizer(input_text, return_tensors="pt").input_ids.to(device)
 
 
