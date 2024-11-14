705629a596f6181a8462de801013615d0fcaa87b
abhisheknair10 <nairabs10@gmail.com>
2024-11-14 17:19:45 -0500

    debug

--- src/main.cu
+++ src/main.cu
@@ -56,7 +56,8 @@ int main() {
     CudaCache *Cache = init_cache(llama3_model);
     CLEAR_TERMINAL();
     while (true) {
-        char *input_str = strdup("<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nYou are a helpful assistant, here to provide clear and concise answers to the user's questions.<|eot_id|><|start_header_id|>user<|end_header_id|>\n\nWhat is the largest ocean in the world?<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n");
+        // char *input_str = strdup("<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nYou are a helpful assistant, here to provide clear and concise answers to the user's questions.<|eot_id|><|start_header_id|>user<|end_header_id|>\n\nWhat is the largest ocean in the world?<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n");
+        char *input_str = strdup("<|begin_of_text|>What is the largest ocean in the world?\n\n");
 
         /*
         char *input_str = (char *)malloc(sizeof(char) * 2048);
