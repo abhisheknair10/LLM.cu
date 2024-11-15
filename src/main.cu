9baf6e3fa4568103566bd46c216fc5086e31b9ef
abhisheknair10 <nairabs10@gmail.com>
2024-11-15 02:09:46 -0500

    debug

--- src/main.cu
+++ src/main.cu
@@ -54,7 +54,7 @@ int main() {
     }
 
     CudaCache *Cache = init_cache(llama3_model);
-    CLEAR_TERMINAL();
+    // CLEAR_TERMINAL();
     while (true) {
         char *input_str = strdup("<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nAs a helpful assistant, answer the user questions with clarity and detail <|eot_id|><|start_header_id|>user<|end_header_id|>\n\nWhat is the largest ocean in the world?<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n");
 
